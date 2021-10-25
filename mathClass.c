1834194f1ceddf567e389be49e104eeb3ac8756b
LIgooNE93 <93137713+LIgooNE93@users.noreply.github.com>
2021-10-25 19:54:07 +0600

    update commentaries
    i'm update commentaris this code

--- mathClass.c
+++ mathClass.c
@@ -1,11 +1,11 @@
-class Math() 
+class Math() // class name
 {
-     class Math () {}
-     int a = 0;
-     int b = 0;
-     int total = 0;
+     class Math () {} //init class
+     int a = 0; // value a
+     int b = 0; // value b
+     int total = 0; // total
      
      public void setA(int a){ this.a = a;} // method this class setup value a
      public void setB(int b){ this.b = b;} // method this class setup value b
-     public int getTotal() {return this.a + this.b;} //
+     public int getTotal() {return this.a + this.b;} // method return total value
 }
