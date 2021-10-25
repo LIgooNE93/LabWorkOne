8daed9b2ef5bf95c74c1864f240d7aacb1fd7db8
LIgooNE93 <93137713+LIgooNE93@users.noreply.github.com>
2021-10-25 19:51:59 +0600

    I'm commented methods class
    update commentaries

---
+++ mathClass.c
@@ -0,0 +1,11 @@
+class Math() 
+{
+     class Math () {}
+     int a = 0;
+     int b = 0;
+     int total = 0;
+     
+     public void setA(int a){ this.a = a;} // method this class setup value a
+     public void setB(int b){ this.b = b;} // method this class setup value b
+     public int getTotal() {return this.a + this.b;} //
+}
